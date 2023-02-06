767c23bd2c52f8ab586ebb54258c0b5839759ff8
nukleus <101455529+hendrikhermawan@users.noreply.github.com>
2023-02-06 16:55:51 +0700

    Create index.php

---
+++ php dasar/pertemuan 1/index.php
@@ -0,0 +1,24 @@
+
+
+<?php
+
+$x = 10;
+
+var_dump($x > 20 && $x % 2 == 0);
+
+$nama = "gerry";
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hallo , <?php echo "hendrik" ?></h1>
+    <h1>hallo , <?php echo $nama ?></h1>
+</body>
+</html>
