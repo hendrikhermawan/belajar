b519b51fc77e0073c97a1ba45ec94e80a7dd6085
hendrikhermawan <hendrikprima97@gmail.com>
2024-01-12 23:12:45 +0700

    keliru login e awokawok sekarang sudah ku tambahi yang sesuai dengan databasenya

--- php dasar/pertemuan14/login.php
+++ php dasar/pertemuan14/login.php
@@ -1,41 +1,49 @@
 <?php 
-// apakah tombol submit sudah di tekan atau belum
-if(isset($_POST["submit"])){
-    // cek username & password
-        if($_POST["username"]=="hendrik" && $_POST["password"]=="123"){
-    // jika benar, redirect ke halaman admin
-        header("Location: admin.php");
-        exit;
-        } else {
-    // jika salah, tampilkan pesan kesalahan
-    $error = true;    
+require 'function.php';
+if (isset($_POST["login"])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
     
+    $result = mysqli_query($db,"SELECT * FROM user WHERE username ='$username'");
+
+    //cek username terlebih dahulu
+    if(mysqli_num_rows($result) == 1){
+        //cek password
+        $row = mysqli_fetch_assoc($result);
+       if( password_verify($password,$row["password"])){
+            header("Location: index.php");
+            exit;
+       }else{
+        
+       }   
+
     }
+    $error = true;
+    
 }
 
-
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>login</title>
 </head>
 <body>
-
-    <!-- judul di dalam web -->
-    <h1>Login Admin</h1> 
-    <!-- buat paragraf untuk menampilkan kesalahan dalam input -->
-    <?php if(isset($error)):?>
-        <p style="color: red;">username / password salah!</p>
-    <?php endif;?>
-    <!-- daftar login -->
-    <ul>
-        <form action="" method="post">
+    <h1>halaman login</h1>
+    <?php if(isset($error)): ?>
+        <p style="color: red;" >username atau password salah</p>
+    <?php endif; ?>
+    <form action="" method="post">
+        <ul>
             <li>
-                <label for="username">username</label>
+                <label for="username">Username</label>
                 <input type="text" name="username" id="username">
             </li>
             <li>
@@ -43,9 +51,9 @@ if(isset($_POST["submit"])){
                 <input type="password" name="password" id="password">
             </li>
             <li>
-                <button type="submit" name="submit">login</button>
+                <button type="submit" name="login">login</button>
             </li>
-        </form>
-    </ul>
+        </ul>
+    </form>
 </body>
 </html>
