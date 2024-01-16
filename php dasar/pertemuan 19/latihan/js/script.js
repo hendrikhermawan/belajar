714a761467704cd24f1f55cffbc8067a6b0a3802
hendrikhermawan <hendrikprima97@gmail.com>
2024-01-16 17:25:25 +0700

    tambah file mahasiswa.php

--- php dasar/pertemuan 19/latihan/js/script.js
+++ php dasar/pertemuan 19/latihan/js/script.js
@@ -1,5 +1,28 @@
+//ambil elemen yang akan digunakan
 var keyword = document.getElementById('keyword');
 var tombol_cari = document.getElementById('tombol-cari');
-var container = document.getElementByI('container');
+var container = document.getElementById('container');
+
+//tambahkan event ketika user menulis di keyword
+
+keyword.addEventListener('keyup', function(){
+
+    // console.log(keyword.value);
+
+    // buat object ajax
+
+    var xhr = new HTMLHttpRequest();
+
+    // cek kesiapan dari ajax
+
+    xhr.onreadystatechange =    function 
+    () {
+     
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            
+        }
+    }
 
 
+
+});
