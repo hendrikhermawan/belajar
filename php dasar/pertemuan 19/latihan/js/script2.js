1befac94b30e789f100596b1cd98b6c2e7cfd5e6
hendrikhermawan <hendrikprima97@gmail.com>
2024-01-25 23:24:36 +0700

    nambah menggunakan get di js

--- php dasar/pertemuan 19/latihan/js/script2.js
+++ php dasar/pertemuan 19/latihan/js/script2.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
+    
+  // hilangkan tombol cari
+  $("#tombol-cari").hide();
   //buat event ketika keyword ditulis
 
   $("#keyword").on("keyup", function () {
-    $("#container").load("ajax/mahasiswa.php?keyword=" + $("#keyword").val());
+
+    // menggunakan load
+    // $("#container").load("ajax/mahasiswa.php?keyword=" + $("#keyword").val());
+  
+    // menggunakan $.get()
+    $.get('ajax/mahasiswa.php?keyword=' + $('#keyword').val(), function(data){
+      $("#container").html(data);
+    })
+  
+  
   });
+
+
+
 });
